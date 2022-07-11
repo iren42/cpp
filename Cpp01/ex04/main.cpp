38657329b2f61fcad61e74c886484aebec5308cb
isabelle <70582634+iren42@users.noreply.github.com>
2022-07-11 11:21:00 +0200

    empty string as invalid parameter for s1

--- Cpp01/ex04/main.cpp
+++ Cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: isabelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 19:32:41 by isabelle          #+#    #+#             */
-/*   Updated: 2022/07/10 19:29:43 by iren             ###   ########.fr       */
+/*   Updated: 2022/07/11 11:17:40 by isabelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <fstream>
 #include <cstring>
 
+//#define DEBUG
 // Returns a string: <filename>.replace
 static std::string	fileDotReplace(const char *s)
 {
@@ -26,32 +27,48 @@ static std::string	fileDotReplace(const char *s)
 
 // copies the content of <filename> in <filename>.replace
 // while replacing every occurence of <s1> with <s2>
-static void	cpy_replace(std::ifstream &ifs, const std::string newFileStr,
+static void	ft_replace(std::ifstream &ifs, const std::string newFileStr,
 		const std::string str1, const std::string str2)
 {
 	const char		*newFile = &newFileStr[0];
 	std::ofstream	ofs(newFile, std::ofstream::out);
-	const int		len = (int)str1.length();
+	int				len = (int)str1.length();
 	std::string		line;
 	std::string		tmp_after;
 	std::string		tmp_before;
-	int	pos;
+	int				pos;
 
+#ifdef DEBUG
+	std::cout << len << std::endl;
+#endif
 	while (std::getline(ifs, line))
 	{
 		pos = (int)line.find(str1);
+#ifdef DEBUG
+		std::cout << pos << std::endl;
+#endif
 		if (pos >= 0)
 		{
 			while (pos >= 0)
 			{
 				tmp_before = line.substr(0, (long unsigned int)pos);
 				tmp_after = line.substr((long unsigned int)(pos + len));
+#ifdef DEBUG
+				std::cout << "tmp_before : " << tmp_before << std::endl;
+				std::cout << "tmp_after : " << tmp_after << std::endl;
+#endif
 				ofs << tmp_before;
 				ofs << str2;
 				pos = (int)tmp_after.find(str1);
-				if (pos < 0)
+				if (pos < 0 || tmp_after == "")
+				{
 					ofs << tmp_after << std::endl;
+					break ;
+				}
 				line = tmp_after;
+#ifdef DEBUG
+				std::cout << "End of loop" << std::endl;
+#endif
 			}
 		}
 		else
@@ -65,7 +82,7 @@ int	main(int ac, char **av)
 	if (ac != 4)
 	{
 		std::cout << "Error: program takes three parameters" << std::endl;
-		std::cout << "./ex04 <filename> <s1> <s2>" << std::endl;
+		std::cout << "./ex04 <filename> <string1> <string2>" << std::endl;
 		return (0);
 	}
 	std::ifstream ifs;
@@ -75,9 +92,14 @@ int	main(int ac, char **av)
 		std::cout << "File is opened" << std::endl;
 
 		const std::string	str1(av[2]);
+		if (str1 == "")
+		{
+			std::cout << "Invalid parameter for <string1>" << std::endl;
+			return (0);
+		}
 		const std::string	str2(av[3]);
 		const std::string	newFileStr = fileDotReplace(av[1]);
-		cpy_replace(ifs, newFileStr, str1, str2);
+		ft_replace(ifs, newFileStr, str1, str2);
 		ifs.close();
 	}
 	else
