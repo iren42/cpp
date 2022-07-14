f298d3c9405130aeae98784f34db727d90859620
iren42 <kalilni1029@gmail.com>
2022-07-14 18:58:15 +0200

    done

--- Cpp07/ex02/Array.hpp
+++ Cpp07/ex02/Array.hpp
@@ -1,12 +1,121 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <iostream>
 
+template <typename T>
 class Array
 {
+	private:
+		T	*_elements;
+		int	_size;
 
+	public:
+		virtual ~Array();
+		Array();
+		Array(int size);
+		Array(const Array &rhs);
+		Array &operator = (const Array &rhs);
+
+		int	getSize() const;
+		T	&operator [] (int position) const;
 
 
 };
 
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const Array<T> &rhs);
+
+/* IMPLEMENTATION */
+
+
+	template <typename T>
+Array<T>::~Array()
+{
+	std::cout << "Array Destructor called" << std::endl;
+	delete [] _elements;
+}
+
+	template <typename T>
+Array<T>::Array() : _elements(0), _size(0)
+
+{
+	std::cout << "Array Default constructor called" << std::endl;
+}
+
+	template <typename T>
+Array<T>::Array(int size) :  _elements(new T[size]), _size(size)
+{
+	std::cout << "Array Default constructor called" << std::endl;
+	int	i = 0;
+	while (i < _size)
+	{
+		_elements[i] = T();
+		i++;
+	}
+}
+
+
+	template <typename T>
+Array<T>::Array(const Array &other)
+{
+	std::cout << "Array Copy constructor called" << std::endl;
+	_size = other._size;
+	_elements = 0;
+	*this = other;
+}
+
+	template <typename T>
+Array<T> &Array<T>::operator=(const Array<T> &other)
+{
+	std::cout << "Array Copy assignment constructor called" << std::endl;
+	if (this != &other)
+	{
+		if (this->_elements != 0)
+			delete [] _elements;
+		_elements = new T[other._size];
+		_size = other._size;
+		int	i = 0;
+		while (i < _size)
+		{
+			_elements[i] = other._elements[i];
+			i++;
+		}
+	}
+	return (*this);
+}
+
+
+template <typename T>
+int	Array<T>::getSize() const
+{
+	return (_size);
+}
+
+template <typename T>
+T &Array<T>::operator [] (int position) const
+{
+	if (position >= _size)
+		throw (std::exception());
+	return (this->_elements[position]);
+}
+
+
+	template <typename T>
+std::ostream &operator<<(std::ostream &os, const Array<T> &rhs)
+{
+	const int	size = rhs.getSize();
+	int	i = 0;
+	os << "[";
+	while (i < size)
+	{
+		os << rhs[i];
+		if (i < size - 1)
+			os << ", ";
+		i++;
+
+	}
+	os << "]";
+	return (os);
+}
 #endif
---
+++ Cpp07/ex02/main.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iren <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 13:22:25 by iren              #+#    #+#             */
+/*   Updated: 2022/07/14 18:56:29 by iren             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <ctime>
+#include <cstdlib>
+#include "Array.hpp"
+#include "../Awesome.hpp"
+# define RT   "\033[0m"
+# define RED     "\033[31m"      /* Red */
+# define GR   "\033[32m"      /* Green */
+# define YEL  "\033[33m"      /* Yellow */
+# define BL    "\033[34m"      /* Blue */
+int	main()
+{
+	srand((unsigned int)time(NULL));
+	{
+		int	i= 0;
+		Array<int> intBigArray(15);
+		while (i < 15)
+		{
+			intBigArray[i] = std::rand();
+			i++;
+		}
+		std::cout << YEL << intBigArray << RT << std::endl;
+		Array<int> intArray(5);
+		std::cout << YEL << intArray << RT << std::endl; // every value is initialized by default 
+		intArray[0] = 10;								// for int , it's 0
+		intArray[1] = 100;
+		intArray[2] = 4;
+		intArray[3] = 78;
+		intArray[4] = 13;
+		std::cout << YEL << intArray << RT << std::endl;
+		std::cout << "values set for intArray" << std::endl;
+		// TEST 1 : deep copy with copy constructor and copy assign constructor
+		{
+			Array<int> intArrCopy(intArray);
+			std::cout << YEL << intArrCopy << RT << std::endl;
+			Array<int> intArrAssign = intArray;
+			std::cout << YEL << intArrAssign << RT << std::endl;
+			intArrCopy[0] = 33;
+			intArrCopy[1] = 33;
+			intArrCopy[2] = 33;
+			std::cout << YEL << intArrCopy << RT << std::endl;
+		}
+		std::cout  << std::endl;
+		std::cout << YEL << intArray << RT << std::endl; // intArray's values are not changed
+		// TEST 1 bis : copy assign from a much bigger array
+		intArray = intBigArray;
+		std::cout << YEL << intBigArray << RT << std::endl;
+		std::cout << YEL << intArray << RT << std::endl; 
+
+	}
+
+	std::cout  << std::endl;
+	std::cout  << std::endl;
+	// TEST 2 : test with std::string
+	{	Array<std::string> strArray(3);
+		std::cout << GR << strArray << RT << std::endl;
+		strArray[0] = "hi "; // set values
+		strArray[1] = "my name is";
+		strArray[2] = " PARIS";
+		std::cout << GR << strArray << RT << std::endl; // */
+
+
+		Array<std::string> strArrayBis = strArray;
+		std::cout << GR << strArrayBis << RT << std::endl;
+		strArrayBis[2] = "NEW YORK";
+		std::cout  << std::endl;
+		std::cout << GR << strArray << RT << std::endl; // */
+		std::cout << GR << strArrayBis << RT << std::endl;
+		//	std::cout << << std::endl;
+		//	std::cout << << std::endl;
+
+	}
+	// TEST 3: access an element out of bounds
+	try
+	{
+		Array<float> fArray(4);
+		fArray[0] = 1.0f;
+		std::cout << BL << fArray << RT << std::endl;
+		fArray[4] = 3.5;
+		std::cout << BL << fArray << RT << std::endl;
+
+	}
+	catch (std::exception &e)
+	{
+		std::cout << RED << "Caught: " << e.what() << RT << std::endl;
+
+	}
+	return (0);
+}
