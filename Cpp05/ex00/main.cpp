ad10b085d21aefb36cc1f094a4749ec42bc76583
iren42 <kalilni1029@gmail.com>
2022-07-14 11:51:21 +0200

    up

--- Cpp05/ex00/main.cpp
+++ Cpp05/ex00/main.cpp
@@ -6,25 +6,48 @@
 /*   By: isabelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 16:35:24 by isabelle          #+#    #+#             */
-/*   Updated: 2022/07/13 18:59:49 by iren             ###   ########.fr       */
+/*   Updated: 2022/07/13 23:06:02 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-
+# define RT   "\033[0m"
+# define RED     "\033[31m"      /* Red */
+# define GR   "\033[32m"      /* Green */
+# define YEL  "\033[33m"      /* Yellow */
 int	main()
 {
+	// TEST 0: instantiate valid Bureaucrats
+	try
+	{
+		//	Bureaucrat a("Too low", 170);
+		Bureaucrat a("iren", 136);
+		Bureaucrat b("iren42", 1);
+		Bureaucrat c("iren43", 150);
+		std::cout << GR << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << c << RT << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << RED << e.what() << RT << std::endl;
+	}
+
+	std::cout << std::endl << std::endl;
+
 	// TEST 1: instantiate a Bureaucrat with invalid grade
 	try
 	{
 		//	Bureaucrat a("Too low", 170);
 		Bureaucrat b("Too high", 0);
+		std::cout << b << std::endl;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RT << std::endl;
 	}
 
+	std::cout << std::endl << std::endl;
 	// TEST 2: increment a Bureaucrat's grade until it is too high
 	try
 	{
@@ -36,16 +59,16 @@ int	main()
 		while (i < 10)
 		{
 			albert.upGrade();
-			std::cout << albert << std::endl;
+			std::cout << GR << albert << RT << std::endl;
 			i++;
 		}
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RT << std::endl;
 	}
 
-
+	std::cout << std::endl << std::endl;
 	// TEST 3: decrement a Bureaucrat's grade until it is too low
 	try
 	{
@@ -60,27 +83,27 @@ int	main()
 		while (i < 30)
 		{
 			josia.downGrade();
-			std::cout << josia << std::endl;
+			std::cout << GR << josia << RT << std::endl;
 			i++;
 		}
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RT << std::endl;
 	}
 
+	std::cout << std::endl << std::endl;
 	// TEST 4: set a grade too high
 	try
 	{
 
 		Bureaucrat	lola("Lola", 140);
 		lola.setGrade(0);
-		std::cout << lola << std::endl;
+		std::cout << GR << lola << RT <<  std::endl;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RT << std::endl;
 	}
-
 	return (0);
 }
--- Cpp07/ex00/main.cpp
+++ Cpp07/ex00/main.cpp
@@ -6,29 +6,41 @@
 /*   By: iren <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 08:57:22 by iren              #+#    #+#             */
-/*   Updated: 2022/07/14 09:10:15 by iren             ###   ########.fr       */
+/*   Updated: 2022/07/14 10:18:25 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+//#include "../Awesome.hpp"
 
 int	main()
 {
-	// TEST 1 : tests from the subject
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	// TEST 1 : test from the subject
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-std::string c = "chaine1";
-std::string d = "chaine2";
+		std::string c = "chaine1";
+		std::string d = "chaine2";
 
-::swap(c, d);
-std::cout << "c = " << c << ", d = " << d << std::endl;
-std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	std::cout << std::endl << std::endl;
+	// TEST 2: test from the guidelines
+/*	{
+		Awesome a(2), b(4);
+		swap(a, b);
+		std::cout << a << " " << b << std::endl;
+		std::cout << max(a, b) << std::endl;
+		std::cout << min(a, b) << std::endl;
+	}
+// */
 	return (0);
 }
