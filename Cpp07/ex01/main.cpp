85ef6b7c9380f5216b55ffb997d1f0138ba16cf3
iren42 <kalilni1029@gmail.com>
2022-07-14 19:06:31 +0200

    up

--- Cpp07/ex01/main.cpp
+++ Cpp07/ex01/main.cpp
@@ -6,22 +6,22 @@
 /*   By: iren <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 11:01:38 by iren              #+#    #+#             */
-/*   Updated: 2022/07/14 11:20:09 by iren             ###   ########.fr       */
+/*   Updated: 2022/07/14 19:05:05 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
-#include "../Awesome.hpp"
+//#include "../Awesome.hpp"
 
 int	main()
 {
 	// TEST FROM GUIDELINES
 	int	tab[] = {0, 1, 2, 3, 4};
-	Awesome	tab2[5];
+//	Awesome	tab2[5];
 
 	iter(tab, 5, print);
 	std::cout << std::endl;
-	iter(tab2, 5, print);
+//	iter(tab2, 5, print);
 
 	return (0);
 }
