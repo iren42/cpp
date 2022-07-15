403a7054ee48c8c52f296cc4635e24e707e46230
isabelle <70582634+iren42@users.noreply.github.com>
2022-07-15 08:05:33 +0200

    up

--- Cpp08/ex02/MutantStack.hpp
+++ Cpp08/ex02/MutantStack.hpp
@@ -5,15 +5,14 @@
 #include <stack>
 
 template <typename T>
-class MutantStack<T> : public std::stack<T>
+class MutantStack : public std::stack<T>
 {
-	using std::stack<T>::c;
 	public:
-	~MutantStack();
-	MutantStack();
-	MutantStack(const MutantStack &other);
-	MutantStack &operator = (const MutantStack &other);
-
+		MutantStack();
+		MutantStack(MutantStack<T> const &rhs);
+		virtual ~MutantStack();
+		MutantStack<T> &operator=(MutantStack<T> const &other);
+/*
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
@@ -23,7 +22,7 @@ class MutantStack<T> : public std::stack<T>
 	iterator	end() { return this->c.end(c); }
 
 	const_iterator	begin() const { return this->c.cbegin(c); }
-	const_iterator	end() const { return this->c.cend(c); }
+	const_iterator	end() const { return this->c.cend(c); }*/
 
 };
 
@@ -46,12 +45,12 @@ MutantStack<T>::MutantStack(const MutantStack<T> &other)
 }
 
 template <typename T>
-MutantStack<T> &MutantStack::operator=(const MutantStack<T> &other)
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &other)
 {
 	std::cout << "MutantStack Copy assignment constructor called" << std::endl;
 	if (this != &other)
 	{
-		std::stack=operator(other);
+		operator=(other);
 	}
 	return (*this);
 }
--- Cpp08/ex02/main.cpp
+++ Cpp08/ex02/main.cpp
@@ -1,9 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isabelle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 07:51:43 by isabelle          #+#    #+#             */
+/*   Updated: 2022/07/15 08:02:17 by isabelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "MutantStack.hpp"
 
 int	main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
+	MutantStack<int> mstack;
+	mstack.push(5);
 
-std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
 }
