b00cde3f098518ebb11ce0dd40565f7b74839324
iren42 <kalilni1029@gmail.com>
2022-07-15 13:41:39 +0200

    T and nt &T

--- Cpp08/ex00/easyfind.hpp
+++ Cpp08/ex00/easyfind.hpp
@@ -8,14 +8,14 @@
 # include <list>
 
 template <typename T>
-void	easyfind(T &container, int tofind);
+void	easyfind(T container, int tofind);
 
 
 
 /* IMPLEMENTATION */
 
 template <typename T>
-void	easyfind(T &container, int tofind)
+void	easyfind(T container, int tofind)
 {
 	if (std::find(container.begin(), container.end(), tofind) != container.end())
 		std::cout << "Found: " << tofind << std::endl;
