7dc1296ef30b168f370ec3db99a8a1e09bc3005b
isabelle <70582634+iren42@users.noreply.github.com>
2023-11-20 21:23:14 +0100

    up: independent parsing function -> static member function

--- Cpp09/ex01/RPN.cpp
+++ Cpp09/ex01/RPN.cpp
@@ -29,3 +29,71 @@ int	RPN::calc(const char* s)
 {
 	return (0);
 }
+
+bool	RPN::parse(const char *s)
+{
+	std::stringstream	ss(s);
+	std::string	word;
+	char	*p = 0;
+
+	/* necessary to use strchr() */
+	std::string	t(TOKENS);
+	char	*tokens = new char[t.size() + 1];
+	strcpy(tokens, t.c_str());
+
+	while (ss >> word)
+	{
+		/* parse tokens */
+		if (word.size() == 1 && is_a_num(word) == false)
+		{
+			p = strchr(tokens, word[0]);
+			/* is it a token? */
+			if (p != 0)
+				std::cout << "token found: " << word[0] << std::endl;
+			else
+			{	
+				/* is it a character? */
+				if (is_a_num(word) == false)
+				{
+					std::cout << word << " is NOT a num" << std::endl;
+					delete tokens;
+					throw std::runtime_error(ERR_NOT_A_RPN_EXPR);	
+				}
+			}
+		}
+		else if (word.size() > 1 && is_a_num(word) == false)
+		{
+			std::cout << word << " is neither a num or a token" << std::endl;
+			delete tokens;
+			throw std::runtime_error(ERR_NOT_A_RPN_EXPR);	
+		}
+		else
+			std::cout << word << std::endl;
+	}
+	delete tokens;
+	return (true);
+}
+
+bool	RPN::is_zero(std::string &s)
+{
+	std::string::iterator	it = s.begin();
+
+	while (it != s.end())
+	{
+		if ((*it) != '0')
+			return (false);
+		it++;
+	}
+	return (true);
+}
+
+bool	RPN::is_a_num(std::string &word)
+{
+	int	num = atoi(word.c_str());
+
+	std::cout << num << " num, strcmp: " << strcmp(word.c_str(), "0") << std::endl;
+	if (num == 0 && is_zero(word) == false)
+		return (false);
+	return (true);
+}
+
--- Cpp09/ex01/RPN.hpp
+++ Cpp09/ex01/RPN.hpp
@@ -8,20 +8,18 @@
 # include <cstring> // strchr
 # include <exception>
 # include <stdlib.h> // atoi
-//# define DEBUG
 
 # define ERR_ARG "Error: does not have the right number of arguments."
 # define ERR_NOT_A_RPN_EXPR "Error: not a RPN expression."
 
 # define TOKENS "+-*/"
 
-bool	parse(const char *s);
-bool	is_a_num(std::string &word);
-bool	is_zero(std::string &s);
-
 class RPN
 {
-	protected:
+	private:
+		static bool	is_a_num(std::string &word);
+		static bool	is_zero(std::string &s);
+
 
 	public:
 		~RPN();
@@ -29,6 +27,7 @@ class RPN
 		RPN(const RPN &other);
 		RPN &operator = (const RPN &other);
 
+		static bool parse(const char *);
 		int	calc(const char*);
 };
 
